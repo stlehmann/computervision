9e62762bb1715ea97d478fdf64ecea09f7fec488
Stefan Lehmann <stlm@posteo.de>
2020-03-01 22:26:17 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='computervision',
+    version='0.1.0',
+    packages=['computervision'],
+    package_dir={'': 'src'},
+    scripts=["scripts/motion-detection_mog2.py"],
+    url='',
+    license='MIT',
+    author='Stefan Lehmann',
+    author_email='stlm@posteo.de',
+    description='some computervision examples'
+)
