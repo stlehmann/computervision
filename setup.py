25cc0b72fe8659bafbe40d2c1259f47826de50c5
Stefan Lehmann <stlm@posteo.de>
2020-04-10 16:37:36 +0200

    Improve combine_rects()

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.1.0',
     packages=['computervision'],
     package_dir={'': 'src'},
-    scripts=["scripts/motion-detection_mog2.py"],
+    scripts=["scripts/motion-detection_mog2.py", "scripts/motion-capture.py"],
     url='',
     license='MIT',
     author='Stefan Lehmann',
     author_email='stlm@posteo.de',
     description='some computervision examples',
-    install_requires=['imutils', 'cv2'],
+    install_requires=['imutils'],
 )
--- src/computervision/utils.py
+++ src/computervision/utils.py
@@ -7,7 +7,7 @@ Rect = namedtuple("Rect", "x y w h")
 RectUnion = Union[Tuple[int, int, int, int], Rect]
 
 
-def rects_overlap(r1: Rect, r2: Rect, g: int = 0) -> bool:
+def _rects_overlap(r1: Rect, r2: Rect, g: int = 0) -> bool:
     """Check if two rectangles overlap / touch each other.
 
     :param r1: first rectangle
@@ -24,38 +24,37 @@ def rects_overlap(r1: Rect, r2: Rect, g: int = 0) -> bool:
     )
 
 
+def _create_combined_rect(r0, r1):
+    x0, y0, w0, h0 = r0
+    x1, y1, w1, h1 = r1
+    new_x0, new_y0 = min(x0, x1), min(y0, y1)
+    new_x1, new_y1 = max(x0 + w0, x1 + w1), max(y0 + h0, y1 + h1)
+    return Rect(new_x0, new_y0, new_x1 - new_x0, new_y1 - new_y0)
+
+
 def combine_rects(rects: List[RectUnion], gap: int = 0) -> List[Rect]:
     """Combine overlapping and adjacent rectangles to bigger rectangles.
-
     :param rects: List of rectangles
     :param gap: Maximum gap between rectangles for combining
     :return: List of combined rectangles
-
     """
-    overlapping_rects = set()
-    res: List[Rect] = []
+    rects: List[Rect] = [Rect(x, y, w, h) for (x, y, w, h) in rects]
 
     # find overlapping rects
-    for i0, (x0, y0, w0, h0) in enumerate(rects):
-        overlaps = False
-        for i1, (x1, y1, w1, h1) in enumerate(rects):
-            if i0 == i1:
-                continue
-            r0 = Rect(x0, y0, w0, h0)
-            r1 = Rect(x1, y1, w1, h1)
+    overlap = True
+    while overlap:
+        overlap = False
+        for r0 in rects:
+            for r1 in rects:
+                if r0 == r1:  # skip same rectangles
+                    continue
 
-            if rects_overlap(r0, r1, g=gap):
-                overlaps = True
-                overlapping_rects.add((min(i0, i1), max(i0, i1)))
-
-        if not overlaps:
-            res.append((x0, y0, w0, h0))
-
-    for i0, i1 in overlapping_rects:
-        x0, y0, w0, h0 = rects[i0]
-        x1, y1, w1, h1 = rects[i1]
-        new_x0, new_y0 = min(x0, x1), min(y0, y1)
-        new_x1, new_y1 = max(x0 + w0, x1 + w1), max(y0 + h0, y1 + h1)
-        res.append(Rect(new_x0, new_y0, new_x1 - new_x0, new_y1 - new_y0))
-
-    return res
+                if _rects_overlap(r0, r1, g=gap):
+                    rects.remove(r0)
+                    rects.remove(r1)
+                    rects.append(_create_combined_rect(r0, r1))
+                    overlap = True
+                    break
+            if overlap:
+                break
+    return rects
